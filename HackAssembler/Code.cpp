4b4681660649cb84a6c312282d48ca70f3fefab4
thisisjacob <jacobholfelder@gmail.com>
2020-07-07 20:13:13 -0400

    Added jump conversion to binary

--- HackAssembler/Code.cpp
+++ HackAssembler/Code.cpp
@@ -32,3 +32,30 @@ std::vector<unsigned int>& Code::comp(const std::string& command) {
 
 }
 */
+
+std::vector<unsigned int> Code::jump(const std::string& command) {
+	if (command.compare("JGT") == 0) {
+		return std::vector<unsigned int>{ 0, 0, 1 };
+	}
+	else if (command.compare("JEQ") == 0) {
+		return std::vector<unsigned int>{ 0, 1, 0 };
+	}
+	else if (command.compare("JGE") == 0) {
+		return std::vector<unsigned int>{ 0, 1, 1 };
+	}
+	else if (command.compare("JLT") == 0) {
+		return std::vector<unsigned int>{ 1, 0, 0 };
+	}
+	else if (command.compare("JNE") == 0) {
+		return std::vector<unsigned int>{ 1, 0, 1 };
+	}
+	else if (command.compare("JLE") == 0) {
+		return std::vector<unsigned int>{ 1, 1, 0 };
+	}
+	else if (command.compare("JMP") == 0) {
+		return std::vector<unsigned int>{ 1, 1, 1 };
+	}
+	else {
+		return std::vector<unsigned int>{ 0, 0, 0 };
+	}
+}
